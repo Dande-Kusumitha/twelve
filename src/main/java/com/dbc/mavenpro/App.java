b842d4bc7ae00606d5495dff18309496bd7d03a0
Kusumitha-Dande <kusumitha.dande@gmail.com>
2023-12-06 20:41:58 +0530

    hi commit

---
+++ src/main/java/com/dbc/mavenpro/App.java
@@ -0,0 +1,13 @@
+package com.dbc.mavenpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
